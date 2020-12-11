6732aa7d3596344332055dfa57a738a7caa7fcad
JoniKoils <74239164+JoniKoils@users.noreply.github.com>
2020-12-11 19:31:43 +0500

    Create table_message.sql

---
+++ table_message.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `messages` (
+  `user_id` varchar(200) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `message_id` varchar(200) NOT NULL,
+  `text` varchar(200) NOT NULL,
+  `date` varchar(200) NOT NULL,
+  `ndate` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
