deb3fc25cacec86e95f081141dbc670892bc6061
JoniKoils <74239164+JoniKoils@users.noreply.github.com>
2020-12-11 19:32:32 +0500

    Create table_users.sql

---
+++ table_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` varchar(200) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `status` varchar(200) NOT NULL,
+  `date_begin` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=cp1251 COMMENT='Пользователи';
